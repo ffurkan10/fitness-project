766731f63d7fc9208d170c42fb0026eeb6f0ec72
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2025-07-19 16:45:47 +0300

    Update notificationController.js

--- server/controllers/notificationController.js
+++ server/controllers/notificationController.js
@@ -99,7 +99,10 @@ exports.markNotificationAsRead = catchAsync(async (req, res, next) => {
 
     res.status(200).json({
         status: 'success',
-        message: 'Bildirim okundu olarak işaretlendi.'
+        message: 'Bildirim okundu olarak işaretlendi.',
+        data: {
+            notification
+        }
     });
 });
 
