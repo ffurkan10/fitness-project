e8660f96ad2f6b55856743b4972764442ec6acf3
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2025-08-07 19:29:08 +0300

    update

--- client/src/components/ui/buttons/Button.js
+++ client/src/components/ui/buttons/Button.js
@@ -1,7 +1,7 @@
 
 const Button = ({text, handleClick, width}) => {
   return (
-    <button onClick={handleClick} style={{width: width}} className='text-sm font-medium bg-black text-white px-5 py-3 rounded-md hover:bg-dark transition duration-300'>
+    <button onClick={handleClick} style={{width: width}} className='text-sm font-medium bg-white text-black border border-black px-5 py-3 rounded-md hover:bg-black hover:text-white transition duration-300'>
         {text}
     </button>
   )
