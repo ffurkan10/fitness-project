f780dbe0f7cd5329e3914cd4a3aad33577e4b284
Fatih Furkan Kocabiyik <82720725+ffurkan10@users.noreply.github.com>
2025-08-07 19:31:49 +0300

    Update AddUserButton.js

--- client/src/components/ui/buttons/AddUserButton.js
+++ client/src/components/ui/buttons/AddUserButton.js
@@ -7,7 +7,7 @@ const AddUserButton = () => {
   const dispatch = useDispatch();
 
   return (
-    <button onClick={() => dispatch(showModal("addNewUser"))} className='border border-light flex items-center bg-cardBg backdrop-blur-[20px] px-4 py-2 rounded-md hover:bg-light transition duration-300'>
+    <button onClick={() => dispatch(showModal("addNewUser"))} className='border border-light flex items-center bg-cardBg backdrop-blur-[20px] px-4 py-2 rounded-md '>
         <span className='text-md font-semibold'>
             Üye Ekle  
         </span>
